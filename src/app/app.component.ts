7e539c11c3510615be891cbc70f67de8d44f979f
Cheng Liang <devbean@outlook.com>
2019-12-26 09:12:21 +0800

    Add cascade drop down buttons.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'sandcode';
+export class AppComponent implements OnInit {
+
+  selectedScope: string;
+  selectedMatchType: string;
+
+  scopes = [ 'Keyword', 'Title', 'Content' ];
+  types: string[] = [];
+
+  ngOnInit(): void {
+    setTimeout(() => { // This might be set by network so timeout() used.
+      this.selectedScope = this.scopes[0];
+    }, 2000);
+  }
+
+  onScopeChanged(scope) {
+    this.types = this.getMatchType(scope);
+    this.selectedMatchType = this.types[0];
+    console.log(scope, this.types);
+  }
+
+  private getMatchType(scope) {
+    switch (scope) {
+      case 'Keyword':
+        return [ 'Full Text', 'Forward', 'Backward' ];
+      case 'Title':
+        return [ 'Any' ];
+      case 'Content':
+        return [ 'Full Text', 'Any' ];
+    }
+  }
+
 }
